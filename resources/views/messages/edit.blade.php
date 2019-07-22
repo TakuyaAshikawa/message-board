68711f171b7ad7822374a2972275de667c35ece1
takuya.ashikawa <mk-ae.rech@hotmail.co.jp>
2019-07-22 17:30:58 +0000

    add column

--- resources/views/messages/edit.blade.php
+++ resources/views/messages/edit.blade.php
@@ -7,6 +7,19 @@
     <h1>id: {{ $message->id }} のメッセージ編集ページ</h1>
     
     {!! Form::model($message, ['route' => ['messages.update', $message->id], 'method' => 'put']) !!}
-    
+        
+        <div class="form-group">
+            {!! Form::label('title', 'タイトル:') !!}
+            {!! Form::text('title', null, ['class' => 'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('content', 'メッセージ:') !!}
+            {!! Form::text('content', null, ['class' => 'form-control']) !!}
+        </div>
+
+        {!! Form::submit('更新', ['class' => 'btn btn-light']) !!}
+
+    {!! Form::close() !!}
 
 @endsectio
