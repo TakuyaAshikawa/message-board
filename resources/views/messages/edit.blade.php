00984498501e1835064a1e3a1ebc26a397264e8b
takuya.ashikawa <mk-ae.rech@hotmail.co.jp>
2019-07-22 16:57:33 +0000

    validation

--- resources/views/messages/edit.blade.php
+++ resources/views/messages/edit.blade.php
@@ -2,21 +2,11 @@
 
 @section('content')
 
+    
+    
     <h1>id: {{ $message->id }} のメッセージ編集ページ</h1>
-
-    <div class="row">
-        <div class="col-6">
-            {!! Form::model($message, ['route' => ['messages.update', $message->id], 'method' => 'put']) !!}
-        
-                <div class="form-group">
-                    {!! Form::label('content', 'メッセージ:') !!}
-                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
-                </div>
-        
-                {!! Form::submit('更新', ['class' => 'btn btn-light']) !!}
-        
-            {!! Form::close() !!}
-        </div>
-    </div>
+    
+    {!! Form::model($message, ['route' => ['messages.update', $message->id], 'method' => 'put']) !!}
+    
 
 @endsectio
